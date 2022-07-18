a59f1b6ffa54c190060d36fd655839d8c5037c02
Guillaume RICARD <guillaume.ricard@mail.com>
2022-07-18 14:01:58 -0400

    server: Enum devices
    List devices and device classes

--- src/kheperon.h
+++ src/kheperon.h
@@ -1,13 +1,37 @@
 #include "kheperon.grpc.pb.h"
 
+#include <cstring>
+
 #include <khepera/khepera.h>
 
+static const int kDefined = 1;
+
 extern "C" {
+static int on_device(const char *name, kb_device_config_t *device,
+                     void *context) {
+  SectionConfig *config = reinterpret_cast<SectionConfig *>(context);
+  DeviceConfig *dev_cfg = config->add_devices();
+  dev_cfg->set_name(name);
+
+  for (int i = 0; i < KB_DEVICE_MAX_CLASS; ++i) {
+    if (device->kclass[i].defined == kDefined) {
+      DeviceClass *kclass = dev_cfg->add_classes();
+      kclass->set_address(device->kclass[i].address);
+      kclass->set_device_name(device->kclass[i].device_name);
+    }
+  }
+
+  return 0;
+}
+
 static int on_section(const char *name, kb_section_config_t *section,
-                       void *context) {
+                      void *context) {
   SectionConfigs *configs = reinterpret_cast<SectionConfigs *>(context);
   SectionConfig *config = configs->add_configs();
   config->set_name(name);
+
+  kb_enum_device(name, on_device, config);
+
   return 0;
 }
 }
